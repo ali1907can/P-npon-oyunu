ae849394ab484083d5001ccfda67c5e2652d7eb9
ali1907can <alicann190712@gmail.com>
2024-06-08 16:21:08 +0300

    pinpon oyunu
     2 d masaüstü oyun

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            pictureBox1 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            timer2 = new System.Windows.Forms.Timer(components);
+            timer3 = new System.Windows.Forms.Timer(components);
+            timer4 = new System.Windows.Forms.Timer(components);
+            timer5 = new System.Windows.Forms.Timer(components);
+            label6 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.Navy;
+            label1.Location = new Point(1, -10);
+            label1.Name = "label1";
+            label1.Size = new Size(10, 515);
+            label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Navy;
+            label2.Location = new Point(1302, -1);
+            label2.Name = "label2";
+            label2.Size = new Size(10, 498);
+            label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.AliceBlue;
+            label3.Location = new Point(1, -1);
+            label3.Name = "label3";
+            label3.Size = new Size(1311, 10);
+            label3.TabIndex = 2;
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.White;
+            label4.Location = new Point(12, 495);
+            label4.Name = "label4";
+            label4.Size = new Size(1300, 10);
+            label4.TabIndex = 3;
+            label4.Click += label4_Click_1;
+            // 
+            // label5
+            // 
+            label5.BackColor = Color.Yellow;
+            label5.Location = new Point(33, 157);
+            label5.Name = "label5";
+            label5.Size = new Size(12, 157);
+            label5.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.baseball_25761_1280;
+            pictureBox1.Location = new Point(89, 205);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(42, 42);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // timer2
+            // 
+            timer2.Tick += timer2_Tick;
+            // 
+            // timer3
+            // 
+            timer3.Tick += timer3_Tick;
+            // 
+            // timer4
+            // 
+            timer4.Tick += timer4_Tick;
+            // 
+            // timer5
+            // 
+            timer5.Tick += timer5_Tick;
+            // 
+            // label6
+            // 
+            label6.BackColor = Color.Yellow;
+            label6.Location = new Point(1283, 157);
+            label6.Name = "label6";
+            label6.Size = new Size(12, 157);
+            label6.TabIndex = 6;
+            label6.Click += label6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1307, 506);
+            Controls.Add(label6);
+            Controls.Add(pictureBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private Label label6;
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,133 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer3.Enabled = true;
+            timer5.Enabled = true;
+
+
+        }
+        Random r = new Random();
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x = label5.Location.X;
+            int y = label5.Location.Y;
+            if (e.KeyCode == Keys.W)
+            {
+                y = y - 20;
+                label5.Location = new Point(x, y);
+
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                y = y + 20;
+                label5.Location = new Point(x, y);
+            }
+            int u = label6.Location.X;
+            int ý = label6.Location.Y;
+            if (e.KeyCode == Keys.Up)
+            {
+                ý = ý - 20;
+                label6.Location = new Point(u, ý);
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                ý = ý + 20;
+                label6.Location = new Point(u, ý);
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int top1 = r.Next(10, 15);
+            pictureBox1.Left = pictureBox1.Left + top1;
+            if (label6.Bottom >= pictureBox1.Top && label6.Top <= pictureBox1.Bottom && label6.Right >= pictureBox1.Left && label6.Left <= pictureBox1.Right)
+            {
+                timer1.Enabled = false; timer2.Enabled = true;
+
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int top1 = r.Next(10, 15);
+
+            pictureBox1.Left = pictureBox1.Left - top1;
+            if (label5.Top <= pictureBox1.Bottom && label5.Bottom >= pictureBox1.Top && label5.Left <= pictureBox1.Right && label5.Right > pictureBox1.Left)
+            {
+                timer2.Enabled = false;
+                timer1.Enabled = true;
+            }
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            int top1 = r.Next(10, 15);
+            pictureBox1.Top = pictureBox1.Top + top1;
+            if (pictureBox1.Bottom >= label4.Top)
+            {
+                timer3.Enabled = false;
+                timer4.Enabled = true;
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            int top1 = r.Next(10, 15);
+            pictureBox1.Top = pictureBox1.Top - top1;
+
+            if (pictureBox1.Top < label3.Bottom)
+            {
+                timer4.Enabled = false;
+                timer3.Enabled = true;
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Left <= label1.Right)
+            {
+                timer5.Enabled = false;
+                pictureBox1.Visible = false;
+                MessageBox.Show("2 . oyuncu kazandý");
+            }
+            if (pictureBox1.Right >= label2.Left)
+            {
+                timer5.Enabled = false; pictureBox1.Visible
+                    = false;
+                MessageBox
+                        .Show("1. oyuncu kazandý");
+            }
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
